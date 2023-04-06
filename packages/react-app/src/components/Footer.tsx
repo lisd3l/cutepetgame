17ea45044dc8ef54787ba2bb2394e03c5896f85a
li.tfeng <li.tfeng0602@gmail.com>
2023-04-06 16:15:16 +0800

    changetoJapanese

--- packages/react-app/src/components/Footer.tsx
+++ packages/react-app/src/components/Footer.tsx
@@ -8,24 +8,24 @@ export default function Footer() {
       <img src={PetGif} alt="Pet walk" className="pet-logo" />
       <div className="font-rb2-b text-6md 2xl:text-4md lg:w-full sm:mb-8">ConPet</div>
       <div className="text-2xs 2xl:text-lg lg:text-md sm:mb-8">
-        Slogan slogan slogan. Slogan slogan
+        飼い主となる前に、その動物を最後まで飼えるのか、真剣に考えよう。
         <br />
-        Slogan slogan slogan.
+        飼い主は動物がその命を終えるまで、愛情をもって飼い続けよう。
         <br />
-        Slogan slogan slogan. Slogan slogan slogan.
+        犬猫の幸せのため、近隣に迷惑をかけないため、きちんとしつけをしよう。
         <br />
         <div className="flex items-center mt-6">
-          <a href="/" className="media-icon">
-            <Icon name="facebook" size={24} />
-          </a>
-          <a href="/" className="media-icon">
-            <Icon name="youtube" size={24} />
-          </a>
-          <a href="/" className="media-icon">
+        //  <a href="/" className="media-icon">
+        //    <Icon name="facebook" size={24} />
+        //  </a>
+        //  <a href="/" className="media-icon">
+        //    <Icon name="youtube" size={24} />
+        //  </a>
+          <a href="https://twitter.com/CONPETBATTLE?s=20" className="media-icon">
             <Icon name="twitter" size={24} />
           </a>
           <a href="/" className="media-icon">
-            <Icon name="instagram" size={24} />
+            <Icon name="discord" size={24} />
           </a>
         </div>
       </div>
